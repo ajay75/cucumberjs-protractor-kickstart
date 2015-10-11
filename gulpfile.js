91692d40d8adac3ac4828c671b00b4da637fda01
jaffamonkey <paullittlebury@gmail.com>
2015-10-11 20:05:39 +0200

    Code tidy

--- gulpfile.js
+++ gulpfile.js
@@ -7,43 +7,31 @@ var reporter = require('gulp-protractor-cucumber-html-report');
 var cucumber = require('gulp-cucumber');
 
 var options = {
-  tmp: '.tmp',
-  e2e: 'e2e',
-  e2e_report_dir: 'e2e/reports/',
-  rev: new Date().getTime(),
-  errorHandler: function(title) {
-    return function(err) {
-      gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
-      this.emit('end');
-    };
-  },
-  wiredep: {
-    directory: 'bower_components',
-    exclude: [
-      'bower_components/jquery'
-    ]
-  }
+    tmp: '.tmp',
+    e2e: 'e2e',
+    e2e_report_dir: 'e2e/reports/',
+    rev: new Date().getTime(),
+    errorHandler: function (title) {
+        return function (err) {
+            gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+            this.emit('end');
+        };
+    },
+    wiredep: {
+        directory: 'bower_components',
+        exclude: [
+            'bower_components/jquery'
+        ]
+    }
 };
 
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
-  return (/\.(js)$/i).test(file);
-}).map(function(file) {
-  require('./gulp/' + file)(options);
-});
-
-gulp.task('default', ['clean'], function() {
-  gulp.start('build');
+wrench.readdirSyncRecursive('./gulp').filter(function (file) {
+    return (/\.(js)$/i).test(file);
+}).map(function (file) {
+    require('./gulp/' + file)(options);
 });
 
 gulp.src('./reports/json/cucumber-test-results.json')
     .pipe(reporter({
-      dest: options.e2e + '/reports/html'
+        dest: options.e2e + '/reports/html'
     }));
-
-gulp.task('test', function() {
-  return gulp.src('e2e/features/api.feature')
-      .pipe(cucumber({
-        'steps': ['e2e/step-definitions/api/*.js', 'node_modules/apickli/apickli-gherkin.js'],
-        'format': 'pretty'
-      }));
-});
