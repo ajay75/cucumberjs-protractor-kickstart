377f8dcbaef916554b86da6bd2224b4f113bb267
Paul Littlebury <paul@jaffamonkey.com>
2016-07-15 13:32:38 +0100

    Update parallel.conf.js

--- parallel.conf.js
+++ parallel.conf.js
@@ -4,29 +4,26 @@ exports.config = {
     // The address of a running selenium server.
     rootElement: '[ng-app]',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['feature1.feature',
-        'feature2.feature'
-    ],
+
     multiCapabilities: [{
         'browserName': 'chrome',
         shardTestFiles: true,
-        maxInstances: 4,
+        maxInstances: 3,
         specs: ['e2e/features/ui/*.feature'
         ]
     }],
 
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: 'e2e/features/ui/*.feature',
     framework: 'custom',
     frameworkPath: require.resolve('protractor-cucumber-framework'),
-    chromeOnly: false,
-    baseUrl: '[enter url]',
-
     cucumberOpts: {
         require: [
             'e2e/step-definitions/*.js',
             'e2e/support/*.js'
         ],
         format: 'pretty',
-        tags: '@torun'
-    }
-
+        tags: '@ui'
+    },
 };
