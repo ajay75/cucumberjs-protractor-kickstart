98161b9808366467d51694e25b57387f066d6036
Paul Littlebury <paul@jaffamonkey.com>
2016-07-27 15:54:33 +0100

    Fixed screenshot issue relating to updated nodejs version

--- e2e/support/cucumber-hooks.js
+++ e2e/support/cucumber-hooks.js
@@ -10,10 +10,10 @@ module.exports = function () {
         browser.manage().deleteAllCookies();
     });
 
-    this.After(function (scenario, callback) {
+     this.After(function (scenario, callback) {
         if (scenario.isFailed()) {
             browser.takeScreenshot().then(function (png) {
-                var decodedImage = new Buffer(png, 'base64').toString('binary');
+	            var decodedImage = new Buffer(png.replace(/^data:image\/(png|gif|jpeg);base64,/,''), 'base64');
                 scenario.attach(decodedImage, 'image/png');
                 callback();
             });
